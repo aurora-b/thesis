fcda4f005268f6195458dc782e61debe2efa96ee
zorabf <30967299+zorabf@users.noreply.github.com>
2018-03-15 16:25:30 -0400

    moreimportantfiles

---
+++ ode45try2.m
@@ -0,0 +1,33 @@
+function udot=ode45try2(t,u)
+
+n=512; %grid points
+b=2*pi; %length of x axis
+delx= b/n; %width of space step
+visc=delx^2/8; %viscosity coefficient
+
+for i=1:n
+    if i==1
+%         udot(i,1)= -u(1) * (u(2)-u(n))/(2*delx) + visc * (u(2)-2*u(1)+u(n))/delx^2;
+        udot(i,1)= -u(1) * (u(2)-u(n-1))/(2*delx) + visc * (u(2)-2*u(1)+u(n-1))/delx^2;
+    elseif i<n
+        udot(i,1) = -u(i)*(u(i+1)-u(i-1))/(2*delx) + visc*(u(i+1)-2*u(i)+u(i-1))/delx^2;
+    elseif i==n
+          udot(i,1)= -u(n)*(u(1)-u(n-1))/(2*delx) + visc*(u(1)-2*u(n)+u(n-1))/delx^2;
+%           udot(i,1)=u(1,1)
+    end
+end
+
+        
+
+%     udot(1) =  u(1) * (u(2)-u(n))/(2*delx) + visc * (u(2)-2*u(1)+u(n))/delx^2;
+%     
+%     for i=2:n-1
+%         udot(i) = u(i)*(u(i+1)-u(i-1))/(2*delx) + visc*(u(i+1)-2*u(i)+u(i-1))/delx^2 ; %from ui=ui+udot(ui)
+%     end
+%    
+%     udot(n)= (u(n)*(u(1)-u(n-1)))/(2*delx) + visc*(u(1)-2*u(n)+u(n-1))/delx^2 ;
+
+
+
+% put ode45 in a separate file
+% [t,u]=ode45(@ex6_ode,0:delt:tend,u,[],visc);
---
+++ odetry3.m
@@ -0,0 +1,59 @@
+%Associated with function file 'ode45try2'
+tend=0.9;
+
+n=512; %grid points
+b=2*pi; %length of x axis
+delx= b/n; %width of space stept
+
+delt=0.1*delx;
+visc=delx^2/8
+
+x= 0:delx:b-delx; %adds delx each time and specifies grid points
+uinit=zeros(1,n); %preallocating u
+%%
+for i=1:n
+    uinit(i)= sin(x(i));
+end
+%%
+[t,u]=ode45(@ode45try2,0:delt:tend,uinit);
+
+j=length(u(:,1))
+% 
+% final = [u(j,:)]
+% middle = [u(j-10,:)]
+
+%%
+%Create a movie
+for time=1:j
+    plot(x,u(time,:));
+    title({['1-D Burgers'' equation (\nu = ',num2str(visc),')'];['time(\itt) = ',num2str(delt*time)]});
+    grid on;
+axis([0,b,-1.5,1.5]);pause(.1)
+mov(time)=getframe(figure(1));
+end
+
+
+% v = VideoWriter('burgerrk4.avi');
+% v.FrameRate = 100;  % Default 30
+% v.Quality = 100;    % Default 75
+% open(v)
+% writeVideo(v,mov)
+% close(v)
+%%
+%Display just a final plot
+plot(x/pi,u(j,:))
+grid on;
+axis([0 2 -1.5 1.5])
+xlabel('x/pi')
+ylabel('u(x)')
+%%
+%plot with x=pi line
+h = plot(x,u(j,:)); grid on; axis([0 2*pi -1.5 1.5])
+hold on
+piy=[-1.5 1.5]
+pix= [pi pi]
+plot(pix,piy)
+xlabel('x')
+ylabel('u(x)')
+
+
